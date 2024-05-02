1c0c81cf940d9fe41697052c59e6a251526e82c6
talhayaseen57 <talhayaseen5725@gmail.com>
2024-05-02 21:15:23 +0200

    resolved secret reveal

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5555;
-
-export const MONGODB_URL =
-  "mongodb+srv://root:root@book-store-mern-app.yhckrtd.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN-App";
