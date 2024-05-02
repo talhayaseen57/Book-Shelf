9eb3781e0b1165e8d6d499c1239e278a0e159cd3
talhayaseen57 <talhayaseen5725@gmail.com>
2024-05-02 15:51:18 +0200

    first HTTP route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(574).send("Welcome to Talha's first MERN stack application.");
+})
 app.listen(PORT, () => {
     console.log(`Applicaiton is listening to the port: ${PORT}`);
 });
