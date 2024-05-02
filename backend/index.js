7e3ae822411d22eaa6c9cacf7f4cb684a86fa367
talhayaseen57 <talhayaseen5725@gmail.com>
2024-05-02 20:18:20 +0200

    connected to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,24 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, MONGODB_URL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log(req);
-    return res.status(574).send("Welcome to Talha's first MERN stack application.");
-})
-app.listen(PORT, () => {
-    console.log(`Applicaiton is listening to the port: ${PORT}`);
+app.get("/", (req, res) => {
+  console.log(req);
+  return res
+    .status(574)
+    .send("Welcome to Talha's first MERN stack application.");
 });
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    console.log("The application is successfully connectied to MONGODB.");
+    app.listen(PORT, () => {
+      console.log(`Applicaiton is listening to the port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
