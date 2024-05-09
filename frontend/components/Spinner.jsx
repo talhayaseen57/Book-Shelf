0c0d785f10069dcad95f43d09ae8699dcbee259e
talhayaseen57 <talhayaseen5725@gmail.com>
2024-05-09 18:35:40 +0200

    created home page

---
+++ frontend/components/Spinner.jsx
@@ -0,0 +1,9 @@
+// import React from 'react';
+
+const Spinner = () => {
+  return (
+    <div className='animate-ping w-14 h-14 m-10 rounded-full bg-sky-600'></div>
+  )
+};
+
+export default Spinner;
