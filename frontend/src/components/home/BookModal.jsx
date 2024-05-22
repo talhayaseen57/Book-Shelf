e846a722d52f1bd192f81a195d7627aab9a1e0a3
talhayaseen57 <talhayaseen5725@gmail.com>
2024-05-22 12:53:47 +0200

    updated home card view

--- frontend/src/components/home/BookModal.jsx
+++ frontend/src/components/home/BookModal.jsx
@@ -33,9 +33,7 @@ const BookModal = ({ book, onClose }) => {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quia
                     voluptatum sint. Nisi impedit libero eveniet cum vitae qui expedita
                     necessitatibus assumenda laboriosam, facilis iste cumque a pariatur
-                    nesciunt cupiditate voluptas? Quis atque earum voluptate dolor nisi
-                    dolorum est? Deserunt placeat cumque quo dicta architecto, dolore
-                    vitae voluptate sequi repellat!
+                    nesciunt cupiditate voluptas?
                 </p>
             </div>
         </div>
--- frontend/src/components/home/SingleBookCard.jsx
+++ frontend/src/components/home/SingleBookCard.jsx
@@ -13,18 +13,19 @@ const SingleBookCard = ({ book }) => {
     return (
         <div
             key={book._id}
-            className="border-2 border-gray-500 rounded-lg px-3 py-1 m-3 relative hover:shadow-xl"
+            className="border-2 border-gray-400 bg-gray-400 rounded-lg px-3 py-1 m-3 relative hover:cursor-pointer hover:shadow-xl"
+            onClick={() => setShowModal(true)}
         >
-            <h2 className="absolute top-1 right-2 px-5 py-3 bg-red-400 rounded-lg">
+            <h2 className="absolute top-1 right-2 px-5 py-3 bg-gray-800 text-white rounded-lg">
                 {book.publishYear}
             </h2>
-            <h4 className="my-2 text-gray-500">{book._id}</h4>
+            <h4 className="my-2 text-gray-600">{book._id}</h4>
             <div className="flex justify-start items-center gap-x-2">
-                <PiBookOpenTextLight className="text-red-300 text-2xl" />
+                <PiBookOpenTextLight className="text-red-500 font-bold text-2xl" />
                 <h2 className="my-1">{book.title}</h2>
             </div>
             <div className="flex justify-start items-center gap-x-2">
-                <BiUserCircle className="text-red-300 text-2xl" />
+                <BiUserCircle className="text-red-500 text-2xl" />
                 <h2 className="my-1">{book.author}</h2>
             </div>
             <div className="flex justify-between items-center gap-x-2 mt-3 p-3">
