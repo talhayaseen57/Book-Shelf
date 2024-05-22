3f942de9d201001027cdc66b6f18b636b264da34
talhayaseen57 <talhayaseen5725@gmail.com>
2024-05-22 14:14:59 +0200

    refactored index.html

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 // import React from 'react';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import CreateBook from './pages/CreateBook';
 import DeleteBook from './pages/DeleteBook';
 import Home from './pages/Home';
